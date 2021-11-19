2d4d0cf321717bc5ca8ccbaae29dc39480d75f28
Valentina <galaxyspevv@gmail.com>
2021-11-19 20:43:21 +0200

    Implemented Flash Chat functionality

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Ungurean Valentina on 18.11.2021.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
